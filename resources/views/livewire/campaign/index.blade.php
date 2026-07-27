8abcc7670438f9f649e5167a5dfc02ab4e321aa3
Josias Bueno <messias_jpb@hotmail.com>
2026-07-27 13:25:06 -0300

    Correct campaign index UI

--- resources/views/livewire/campaign/index.blade.php
+++ resources/views/livewire/campaign/index.blade.php
@@ -14,26 +14,26 @@
                     <a href="{{ route('campaigns.show', $campaign) }}" class="font-semibold flex-1 pb-1">{{ $campaign->name }}</a>
                     <x-badge
                         :text="$campaign->is_active ? 'Ativa' : 'Inativa'"
-                        :color="$campaign->is_active ? 'primary' : 'neutral'"
+                        :color="$campaign->is_active ? 'green' : 'neutral'"
                         light />
                 </div>
-                <p class="text-gray-500 text-sm">{{ $campaign->description }}</p>
+                <p class="text-gray-500 dark:text-gray-300 text-sm">{{ $campaign->description }}</p>
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 text-sm gap-3">
                     <div class="flex flex-col">
-                        <span class="font-semibold text-gray-700">Prazo de confirmação</span>
-                        <span class="text-gray-500">{{ $campaign->confirmation_deadline->format('d/m/Y') }}</span>
+                        <span class="font-semibold text-gray-700 dark:text-gray-200">Prazo de confirmação</span>
+                        <span class="text-gray-500 dark:text-gray-300">{{ $campaign->confirmation_deadline->format('d/m/Y') }}</span>
                     </div>
                     <div class="flex flex-col">
-                        <span class="font-semibold text-gray-700">Prazo de entrega</span>
-                        <span class="text-gray-500">{{ $campaign->delivery_deadline->format('d/m/Y') }}</span>
+                        <span class="font-semibold text-gray-700 dark:text-gray-200">Prazo de entrega</span>
+                        <span class="text-gray-500 dark:text-gray-300">{{ $campaign->delivery_deadline->format('d/m/Y') }}</span>
                     </div>
                     <div class="flex flex-col">
-                        <span class="font-semibold text-gray-700">Itens</span>
-                        <span class="text-gray-500">{{ $campaign->items_count }}</span>
+                        <span class="font-semibold text-gray-700 dark:text-gray-200">Itens</span>
+                        <span class="text-gray-500 dark:text-gray-300">{{ $campaign->items_count }}</span>
                     </div>
                     <div class="flex flex-col">
-                        <span class="font-semibold text-gray-700">Sacolas</span>
-                        <span class="text-gray-500">{{ $campaign->bags_count }}</span>
+                        <span class="font-semibold text-gray-700 dark:text-gray-200">Sacolas</span>
+                        <span class="text-gray-500 dark:text-gray-300">{{ $campaign->bags_count }}</span>
                     </div>
                 </div>
             </x-card>
