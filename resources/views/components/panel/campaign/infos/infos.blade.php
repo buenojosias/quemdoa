283eed75866b248517888b5ac3d4c0deaee800ab
Josias Bueno <messias_jpb@hotmail.com>
2026-08-09 23:38:48 -0300

    refactor: update campaign info display structure for improved readability

--- resources/views/components/panel/campaign/infos/infos.blade.php
+++ resources/views/components/panel/campaign/infos/infos.blade.php
@@ -1,6 +1,18 @@
-<div class="p-4 space-y-3 rounded-lg border border-gray-200 dark:border-gray-500">
+<div class="p-4 space-y-4 rounded-lg border border-gray-200 dark:border-gray-500">
     @forelse($this->infos as $info)
-        <x-inline-info :label="$info->title" :value="$info->content" />
+        {{-- <x-inline-info :label="$info->title" :value="$info->content" /> --}}
+        <div class="flex gap-4 text-sm items-center">
+            <div class="flex-1">
+                <p class="text-gray-800 dark:text-gray-300 font-medium">{{ $info->title }}</p>
+                <p class="text-gray-600 dark:text-gray-400">{{ $info->content }}</p>
+            </div>
+            <div class="flex">
+                <x-dropdown icon="ellipsis-vertical" static>
+                    <x-dropdown.items text="Editar" icon="pencil-square" />
+                    <x-dropdown.items text="Excluir" icon="trash" />
+                </x-dropdown>
+            </div>
+        </div>
     @empty
         <p class="py-6 text-center text-gray-500">Nenhuma informação adicionada<br>
             <span class="text-sm">Adicione mais informações sanar as dúvidas os participantes da campanha.</span>
